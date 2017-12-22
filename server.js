630009405e812bb416cc09b0308c47f454a2b3a0
prasadkjose <prasadkjose@gmail.com>
2017-12-22 19:13:47 +0530

    variable port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const hbs = require('hbs');
-var port = process.env.PORT || 3000;
 var app = express();
+app.set( 'port', ( process.env.PORT || 5000 ));
+
 
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
@@ -15,7 +16,6 @@ app.get('/home', (req, res) => {
 });
 
 
-
-app.listen(port, () => {
-  console.log('Server is up on port' + port);
-});
+app.listen( app.get( 'port' ), function() {
+  console.log( 'Node server is running on port ' + app.get( 'port' ));
+  });
