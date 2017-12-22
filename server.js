cf6f5c104af2547b47f95577e53fb54fd1ff8f4e
prasadkjose <prasadkjose@gmail.com>
2017-12-22 20:20:38 +0530

    make it better

--- server.js
+++ server.js
@@ -5,19 +5,40 @@ const fs = require('fs');
 const port = process.env.PORT || 3000;
 var app = express();
 
-
 hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n');
+  next();
+});
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(__dirname + '/public'));
 
 
-app.get('/home', (req, res) => {
+
+app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'ILASA- Intelligent Logistics Allocation System Application',
 
   });
 });
 
+
+// /bad - send back json with errorMessage
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
+  });
+});
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
