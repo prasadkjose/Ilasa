5aa49ed91d0e09dd664706c71f58a7cdce11f82a
prasadkjose <prasadkjose@gmail.com>
2018-01-04 08:39:43 +0530

    updated V1.1

--- server.js
+++ server.js
@@ -9,10 +9,6 @@ hbs.registerPartials(__dirname + '/views/partials')
 app.set('view engine', 'hbs');
 
 
-// app.use((req, res, next) => {
-//   res.render('maintenance.hbs');
-// });
-
 app.use(express.static(__dirname + '/public'));
 
 
